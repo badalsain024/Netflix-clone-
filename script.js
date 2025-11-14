b74f25a0e0b3d2622e2a9b6efd23ffea60ec7561
Badalsain123 <badal.gla_bca24@gla.in>
2025-11-14 21:15:11 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+function openNav(){
+    document.getElementById("myslide").style.width = "2500px";
+    document.getElementById("myslide").style.height = "100px";
+    document.getElementById("main").style.marginLeft = "200px";
+}
+function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+    document.getElementById("main").style.marginLeft= "0";
+  }
